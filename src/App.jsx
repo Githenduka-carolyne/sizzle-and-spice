7c8e3b241ae56283c591e303ac76a16b39bd24ce
carolyne-wanjiru <shirogithenduka@gmail.com>
2024-07-20 18:35:25 +0300

    add the remainig details

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,25 @@ import Menu from "./Pages/Menu/Menu";
 import Home from "./Pages/Home/Home";
 import Signup from "./Pages/Signup/Signup";
 import Login from "./Pages/Login/Login";
+import Admin from "./Pages/Admin/Admin";
+import Addfood from "./Pages/Addfood/Addfood";
+import Editfood from "./Pages/Editfood/Editfood";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
  
   return (
     <>
-      <Header />
       <BrowserRouter>
+        <Header />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/Menu" element={<Menu />} />
           <Route path="/Contacts" element={<Contacts />} />
           <Route path="/Signup" element={<Signup />} />
           <Route path="/Login" element={<Login />} />
+          <Route path="/Admin" element={<Admin />} />
+          <Route path="/Addfood" element={<Addfood />} />
+          <Route path="/Editfood/:id" element={<Editfood />} />
         </Routes>
       </BrowserRouter>
     </>
---
+++ src/Pages/data.js
@@ -0,0 +1,147 @@
+const datainfo = [
+  {
+    type:"Veggie burgers",
+    price: 150,
+    
+  },
+  {
+    type: "Chicken Burgers",
+    price: 300,
+   
+  },
+  {
+    type: "Bacon Cheeseburgers",
+    price: 250,
+
+  },
+  {
+    type: "Classic Burger",
+    price: 160,
+   
+  },
+  {
+    type: "Humburger",
+    price: 200,
+   
+  },
+  {
+    type: "Grilled Chicken wraps",
+    price: 160,
+    
+  },
+  {
+    type: "Chicken Sandwich",
+    price: 270,
+   
+  },
+  {
+    type: "Wrap Sandwich",
+    price: 260,
+   
+  },
+  {
+    type: "Vegetable Sandwich",
+    price: 160,
+   
+  },
+  {
+    type: "Cheese Sandwich",
+    price: 200,
+   
+  },
+  {
+    type: "Traditional cutFries",
+    price: 150,
+   
+  },
+  {
+    type: "Waffle Fries",
+    price: 130,
+   
+  },
+  {
+    
+    type: "Curly Fries",
+    price: 120,
+   
+  },
+  {
+    
+    type: "Potato Wedges",
+    price: 160,
+   
+  },
+  {
+    type: "Masala Chips",
+    price: 180,
+   
+  },
+  {
+    type: "Pasta Alfredo",
+    price: 400,
+   
+  },
+  {
+    type: "Pasta Bolognese",
+    price: 450,
+   
+  },
+  {
+    type: "Spaghetti",
+    price:300,
+  
+  },
+  {
+    type: "Lasagna",
+    price: 450,
+   
+  },
+  {
+    
+    type: "Pasta with pesto",
+    price: 550,
+  },
+   {
+    
+    type: "Grilled Chicken",
+    price: 150,
+  },
+   {
+    type: "Roasted Chicken",
+    price: 250,
+  },
+   {
+    type: "Spicy Wings",
+    price: 350,
+  },
+   {
+    type: "Buffalo wings",
+    price: 450,
+  },
+   {
+    type: "Chicken Lollipops",
+    price: 550,
+  },
+   {
+    type: "Minute Maid(all flavours)-small",
+    price: 80,
+  },
+   {
+    type: "Minute Maid(all flavours)-large",
+    price: 140,
+  },
+   {
+    type: "coke zero sugar-small",
+    price: 80,
+  },
+   {
+    type: "blackcurrent -1 litre",
+    price: 100,
+  },
+   {
+    type: "sprite - litre",
+    price: 100,
+  },
+
+];
+export default datainfo;
